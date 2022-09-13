754838f3194bc5e5ed1d2ec977e5eb1348f240c0
李彦 <844698022@qq.com>
2022-09-13 17:33:53 +0800

    🐞fix: add value prop

--- src/antDesignVue/components/select.ts
+++ src/antDesignVue/components/select.ts
@@ -1,7 +1,7 @@
 export const key = ['select', '下拉', '选择器'];
 export const value = `
 <a-select v-model:value="value1" placeholder="">
-    <a-select-option>option</a-select-option>
+    <a-select-option value="">option</a-select-option>
 </a-select>
 `;
 export const select = {
