63e3a87e84e4686eac4f957fa69cdde0493ba1d8
asnipera <844698022@qq.com>
2022-09-22 00:01:46 +0800

    🏷️chore: remove unused constant

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,7 +1,4 @@
-export const FORM_FLAG = "````";
 export const INLINE_SPLIT = "|";
-export const END_SCRIPT_TAG = "</script>";
-export const CLOSE_SCRIPT_REG = /<\/script>(?!"|')/;
 export const FORM_STATE = "const formState = reactive(object);";
 export const RULES = "const rules = {};";
 export const ANT_DESIGN_VUE = "Ant Design Vue";
